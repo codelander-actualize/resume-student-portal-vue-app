133a66a0e850c13a56aa86587fa762350586be0c
rachel <ferrallr@yahoo.com>
2019-03-11 14:18:20 -0500

    changes to Student Portal welcome in Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>This is your Student Portal</h1>
   </div>
 </template>
 
